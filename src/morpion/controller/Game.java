126821f45644c45921ecc18f7eacbe210c4f4c23
PierreBoue <119578551+PierreBoue@users.noreply.github.com>
2023-01-05 11:47:51 +0100

    corrected a few  mistakes

--- src/morpion/controller/Game.java
+++ src/morpion/controller/Game.java
@@ -3,7 +3,7 @@ package morpion.controller;
 import morpion.model.BoardGame;
 import morpion.model.Cell;
 import morpion.model.Player;
-import morpion.model.TicTacToeBoard;
+//import morpion.model.TicTacToeBoard;
 import morpion.view.ConsoleView;
 import morpion.view.InteractionUtilisateur;
 
@@ -18,8 +18,8 @@ public abstract class Game implements Serializable {
     {
         playDimension = 2;
         InteractionUtilisateur interaction = new InteractionUtilisateur();
-        int taille = getBoardSize();
-        setBoard( taille );
+        int size = getBoardSize();
+        setBoard( size );
         players = new Player[2];
         for (int i=0; i<2; i++)
         {
@@ -39,11 +39,11 @@ public abstract class Game implements Serializable {
     protected abstract void setBoard( int size );
     public void playgame()
     {
-        display();// affiche le plateau vide
+        display();// display empty board
         ConsoleView vue = new ConsoleView();
         Player activePlayer = players[0];
         while ( ! board.isOver())
-        { // boucle principale du jeu
+        { // main game loop
             vue.printMessage("Player " + activePlayer.getColoredSymbol());
             setPlayerNewMove(activePlayer);
             if (activePlayer == players[0])
@@ -52,33 +52,33 @@ public abstract class Game implements Serializable {
         }
 
     }
-    // demande au player passé en argument de jouer et met à jour le plateau avec le coup joué
+    // ask the player passed as argument to play and updates board with player move
     public void setPlayerNewMove( Player player)
     {
         boolean ok = false;
         ConsoleView vue = new ConsoleView();
-        int[] coordonnees = {-1,-1};
+        int[] coordinates = {-1,-1};
         Cell[][] plateau = board.plateau;
         int i=0;
         while (! ok )
         {
-            coordonnees = player.play( board, playDimension ); //récupère le coup joué
-            if (( coordonnees[0] < 0 ) || ( coordonnees[0] >= board.size )) // contrôle de la validité du coup
+            coordinates = player.play( board, playDimension ); //get player move
+            if (( coordinates[0] < 0 ) || ( coordinates[0] >= board.size )) // checks validity of player move
             {
                 vue.printError("Column number should be between 0 and " + ( board.size - 1));
-            } else if (( coordonnees[1] < 0 ) || ( coordonnees[1] >= board.size )) {
+            } else if (( coordinates[1] < 0 ) || ( coordinates[1] >= board.size )) {
                 vue.printError("Line number should be between 0 and " + ( board.size - 1));
-            } else if ( plateau[coordonnees[1]][ coordonnees[0]].owner != null ) {
-                vue.printError("The cell " + coordonnees[0] + " - " + coordonnees[1] + " is already taken, choose another");
+            } else if ( plateau[coordinates[1]][ coordinates[0]].owner != null ) {
+                vue.printError("The cell " + coordinates[0] + " - " + coordinates[1] + " is already taken, choose another");
                 if ( i++ > 10 ) break;
-            } else { // si le coup est valide, attribue au player la case choisie
+            } else { // if valid move sets cell owner to the player
                 ok = true;
-                plateau[coordonnees[1]][coordonnees[0]].owner = player;
+                plateau[coordinates[1]][coordinates[0]].owner = player;
                 display();
             }
         }
     }
-    public void display()// affichage du plateau par la vue
+    public void display()// ask the view to display board
     {
         ConsoleView vue = new ConsoleView();
         vue.displayBoard(board.plateau);
